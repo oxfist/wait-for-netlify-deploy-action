b91cde8676c0e218e9afede570bd162933aaf750
Jason Levy <jason@jlevy.io>
2022-04-17 00:35:56 -0400

    Adds NETLIFY_TOKEN env var for API auth

--- index.js
+++ index.js
@@ -1,88 +1,104 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const axios = require('axios')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+const netlifyToken = process.env.NETLIFY_TOKEN;
+const apiUrl = 'https://api.netlify.com/api/v1/';
 
 const apiGet = async ({ path }) => {
-  const apiUrl = 'https://api.netlify.com/api/v1/'
-  const response = await axios.get(new URL(path, apiUrl).toString())
-  return response.data
-}
+  try {
+    const response = await axios.get(new URL(path, apiUrl).toString(), {
+      headers: {
+        Authorization: `Bearer ${netlifyToken}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    return err.response.data.message || err;
+  }
+};
 
 const getCurrentDeploy = async ({ siteId, sha }) => {
-  const deploys = await apiGet({ path: `sites/${siteId}/deploys` })
-  return deploys.find(
-    (deploy) => deploy.commit_ref === sha && ['production', 'deploy-preview'].includes(deploy.context)
-  )
-}
+  try {
+    const deploys = await apiGet({ path: `sites/${siteId}/deploys` });
+    return deploys.find(
+      (deploy) =>
+        deploy.commit_ref === sha && ['production', 'deploy-preview', 'branch-deploy'].includes(deploy.context)
+    );
+  } catch (err) {
+    return err.response.data.message || err;
+  }
+};
 
 const waitForDeploy = async ({ siteId, sha }) => {
-  const currentDeploy = await getCurrentDeploy({ siteId, sha })
+  const currentDeploy = await getCurrentDeploy({ siteId, sha });
   if (currentDeploy) {
     if (currentDeploy.state === 'ready') {
-      console.log('deploy is ready')
-      return currentDeploy
+      console.log('deploy is ready');
+      return currentDeploy;
     } else if (currentDeploy.state === 'failed') {
-      return null
+      return null;
     } else {
-      await new Promise((r) => setTimeout(r, 2000))
-      return waitForDeploy({ siteId, sha })
+      await new Promise((r) => setTimeout(r, 2000));
+      return waitForDeploy({ siteId, sha });
     }
   } else {
-    return null
+    return null;
   }
-}
+};
 
-const waitForLive = async ({ siteId, sha, MAX_TIMEOUT }) => {
-  const iterations = MAX_TIMEOUT / 2
-  let currentDeploy = null
+const waitForLive = async ({ siteId, sha, MAX_TIMEOUT, headers }) => {
+  const iterations = MAX_TIMEOUT / 2;
+  let currentDeploy = null;
   for (let i = 0; i < iterations; i++) {
     try {
-      currentDeploy = await getCurrentDeploy({ siteId, sha })
+      currentDeploy = await getCurrentDeploy({ siteId, sha, headers });
       if (currentDeploy) {
-        break
+        break;
       } else {
-        await new Promise((r) => setTimeout(r, 2000))
+        await new Promise((r) => setTimeout(r, 2000));
       }
     } catch (e) {
-      console.log(e)
-      core.setFailed('Wait for Netlify failed')
-      return
+      console.log(e);
+      core.setFailed('Wait for Netlify failed');
+      return;
     }
   }
 
   if (!currentDeploy) {
-    core.setFailed(`Can't find Netlify related to commit: ${sha}`)
+    core.setFailed(`Can't find Netlify related to commit: ${sha}`);
   }
 
-  currentDeploy = await waitForDeploy({ siteId, sha })
+  currentDeploy = await waitForDeploy({ siteId, sha });
   if (currentDeploy) {
-    let url = currentDeploy.deploy_ssl_url
+    let url = currentDeploy.deploy_ssl_url;
     // compose permalink URL without Netlify Preview drawer
     if (currentDeploy.context === 'deploy-preview') {
-      url = `https://${currentDeploy.id}--${currentDeploy.name}.netlify.app`
+      url = `https://${currentDeploy.id}--${currentDeploy.name}.netlify.app`;
     }
-    core.setOutput('url', url)
+    core.setOutput('url', url);
   } else {
-    core.setFailed('Netlify deploy error')
+    core.setFailed('Netlify deploy error');
   }
-}
+};
 
 const run = async () => {
   try {
-    const siteId = core.getInput('site_id')
-    const MAX_TIMEOUT = Number(core.getInput('max_timeout')) || 120
+    const siteId = core.getInput('site_id');
 
     if (!siteId) {
-      core.setFailed('Required field `siteId` was not provided')
+      core.setFailed('Required field `siteId` was not provided');
     }
+
+    const MAX_TIMEOUT = Number(core.getInput('max_timeout')) || 120;
 
     const sha = github.context.payload.pull_request
       ? github.context.payload.pull_request.head.sha
-      : github.context.payload.head_commit.id
-    await waitForLive({ MAX_TIMEOUT, siteId, sha })
+      : github.context.payload.head_commit.id;
+    await waitForLive({ MAX_TIMEOUT, siteId, sha, headers });
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
-}
+};
 
-run()
+run();
